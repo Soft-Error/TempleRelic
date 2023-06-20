f1450d1c34ebac35281c3e45e9075b827c549d84
DevZodiac <87861770+Zodiacbits@users.noreply.github.com>
2023-06-20 23:10:36 +0800

    Made tweaks to basic syntax errors.

--- contracts/PathofTheTemplarShard.sol
+++ contracts/PathofTheTemplarShard.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "./PartnerMinter.sol/";
+import "./PartnerMinter.sol";
 
 interface IRelic {
     function balanceOf(address) external returns (uint256);
@@ -19,40 +19,72 @@ interface IShards {
     ) external;
 }
 
-contract PathofTheTemplarShard is ownable {
+contract PathofTheTemplarShard is Ownable {
+    address authorizedMinter;
 
-    event SignedQuestCompletedMessageHash(bytes32 hash);
+event SignedQuestCompletedMessageHash(bytes32 hash);
 
-    IShards private SHARDS,
-    IRelic private RELIC,
+    IShards private SHARDS;
+    IRelic private RELIC;
     uint256[] public SHARD_ID = [1, 2, 3, 4, 5];
 
-mapping (msg.sender => bool) public canMint
-mapping (uint => enclaves) signedQuestCompletedMessage
+    using Counters for Counters.Counter;
+    
+    bytes32 immutable DOMAIN_SEPARATOR;
+
+mapping (msg.sender => bool) public canMint;
+mapping (uint256 => enclaves) signedQuestCompletedMessage;
 
 function mintPathofthetemplarShard() external canMint {
     SHARDS.partnerMint(msg.sender, SHARD_ID, 1, "");
-    require(msg.sender(signedQuestCompletedMessage)) == "true";
 }
 
-function signedQuestCompletedMessage(QuestCompletedMessageReq calldata req, bytes calldata signature) external {
+modifier canMint() {
+    require(msg.sender == authorisedMinter, "You are not allowed to mint");
 
-        bytes32 digest = keccak256(abi.encodePacked(
-            "\x19\x01",
-            DOMAIN_SEPARATOR,
-            hash(req)
+    _;
+}
+
+ function setQuestCompletedMessage(uint256[] calldata signer) external {
+        _setQuestCompletedMessage(msg.sender, signer);
+    }
+
+function _setQuestCompletedMessage(address authorizedMinter, uint256[] calldata signer) internal {
+    if (signer.length > 1) {
+        revert TooManyMessages();
+    }
+
+    questCompletedMessageBy[authorizedMinter] = signer;
+    emit UpdateQuestCompletedMessage(authorizedMinter, signer);
+}
+
+    struct QuestCompletedMessageReq {
+        address signer;
+        uint256 deadline;
+        uint256 nonce;
+    }
+
+function relayedSignQuestCompletedMessageFor(QuestCompletedMessageReq calldata req, bytes calldata signature) external {
+
+    bytes32 digest = keccak256(abi.encodePacked(
+        "\x19\x01",
+        DOMAIN_SEPARATOR,
+        hash(req)
         ));
 
         (address signer, ECDSA.RecoverError err) = ECDSA.tryRecover(digest, signature);
         if (err != ECDSA.RecoverError.NoError) {
-            revert InvalidSignature(req.account);
+            revert InvalidSignature(req.authorizedMinter);
         }
         if (block.timestamp > req.deadline) revert DeadlineExpired(block.timestamp - req.deadline);
-        if (signer != req.account) revert InvalidSignature(req.account);
-        if (_useNonce(req.account) != req.nonce) revert InvalidNonce(req.account);
+        if (signer != req.authorizedMinter) revert InvalidSignature(req.authorizedMinter);
+        if (_useNonce(req.authorizedMinter) != req.nonce) revert InvalidNonce(req.authorizedMinter);
 
-        _setQuestCompletedMessage(req.account, req.discordIds);
+        _setQuestCompletedMessage(req.deadline, req.nonce);
+        emit SignedQuestCompletedMessageHash(digest); 
     }
+
+    event SignedQuestCompletedMessageHash(bytes32 hash);
 
     /**
      * "Consume a nonce": return the current value and increment.
@@ -71,30 +103,21 @@ function signedQuestCompletedMessage(QuestCompletedMessageReq calldata req, byte
 
     bytes32 constant EIP712DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,string version,uint256 chainId)");
 
-    function hash(EIP712Domain memory _input) internal pure returns (bytes32) {    
+    function hash(EIP712Domain memory _input) internal pure returns (bytes32) { 
         return keccak256(abi.encode(
             EIP712DOMAIN_TYPEHASH,
             keccak256(bytes(_input.name)),
             keccak256(bytes(_input.version)),
-            _input.chainId
-            emit SignedQuestCompletedMessageHash(hash);
+            _input.chainId,
         ));
     }
 
-    struct QuestCompletedMessageReq {
-        address account;
-        uint256[] discordIds;
-        uint256 deadline;
-        uint256 nonce;
-    }
-
-    bytes32 constant SIGNEDQUESTCOMPLETEDMESSAGE_TYPEHASH = keccak256("QuestCompletedMessageReq(address account,uint256[] discordIds,uint256 deadline,uint256 nonce)");
+    bytes32 constant SIGNEDQUESTCOMPLETEDMESSAGE_TYPEHASH = keccak256("QuestCompletedMessageReq(address signer,uint256 deadline,uint256 nonce)");
 
     function hash(QuestCompletedMessageReq memory _input) internal pure returns (bytes32) {
         return keccak256(abi.encode(
             SIGNEDQUESTCOMPLETEDMESSAGE_TYPEHASH,
-            _input.account,
-            hash(_input.discordIds),
+            _input.signer,
             _input.deadline,
             _input.nonce
         ));
@@ -103,4 +126,5 @@ function signedQuestCompletedMessage(QuestCompletedMessageReq calldata req, byte
     function hash(uint256[] memory _input) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(_input));
     }
+
 }
