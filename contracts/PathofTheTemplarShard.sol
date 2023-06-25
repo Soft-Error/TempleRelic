1682efbdaf04ea203a34b5bf5efc14c877998a96
DevZodiac <87861770+Zodiacbits@users.noreply.github.com>
2023-06-25 17:31:56 +0800

    Removed redundant imported libraries.

--- contracts/PathofTheTemplarShard.sol
+++ contracts/PathofTheTemplarShard.sol
@@ -1,9 +1,6 @@
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
